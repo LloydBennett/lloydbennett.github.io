d8c3cb12634c4fd84a60b26de9019060cb312f41
Lloyd-Jamie Bennett <lj_bennett@hotmail.co.uk>
2015-09-07 03:23:49 +0100

    masthead styled

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//add all functions here
+
+
+
+$(document).ready(function(){
+  //add all function calls here
+
+
+  $('.nav__menu-icon').on('click', function(){
+    $('.nav__list').slideToggle(300);
+  });
+});
