9fde9a259f4ca879d69b278779dcdc50bca73933
Lloyd-Jamie Bennett <lj_bennett@hotmail.co.uk>
2015-09-07 21:50:44 +0100

    extended responsive navigation

--- js/script.js
+++ js/script.js
@@ -6,7 +6,17 @@ $(document).ready(function(){
   //add all function calls here
 
 
+
+
   $('.nav__menu-icon').on('click', function(){
     $('.nav__list').slideToggle(300);
   });
+
 });
+
+if($(window).resize().width() >= 960){
+  $('.nav__list').appendTo('.nav .wrapper');
+}
+/*else if($(window).width() < 960 && $('.nav__list').parent('.wrapper')) {
+  $('.nav__list').prepend('.header');
+}*/
