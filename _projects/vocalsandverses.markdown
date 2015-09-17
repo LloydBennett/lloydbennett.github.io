c6eca0c38f18fc326553a6bc323f4620d76d3cf8
Lloyd-Jamie Bennett <lj_bennett@hotmail.co.uk>
2015-09-17 01:28:31 +0100

    fixed broken nav menu

---
+++ _projects/vocalsandverses.markdown
@@ -0,0 +1,8 @@
+---
+layout: casestudy
+title:  Vocals & Verses
+category: Web Design
+featured-image: desktopBrowser.jpg
+---
+
+Lorem ipsum.
--- js/script.js
+++ js/script.js
@@ -1,22 +1,76 @@
-//add all functions here
+//variables
+var $navMenu = $('.nav__list'),
+    $ctaButton = $('.btn--primary'),
+    $ctaTarget = $('#cta-target');
+//call to action scrolling functionality
+function scrollTo(handler, target){
+  handler.on('click', function(){
+    $("html, body").animate({
+        scrollTop: target.offset().top
+    }, 600);
+  });
+}
 
-
-
-$(document).ready(function(){
-  //add all function calls here
-
-
-
-
+//dom ready
+$(function(){
+  
+  //mobile menu
   $('.nav__menu-icon').on('click', function(){
     $('.nav__list').slideToggle(300);
   });
+  scrollTo($ctaButton, $ctaTarget);
+});
+
+/* Preparation:
+   - Problem: We dont have a menu for desktop screens.
+   We also want to avoid using any positioning hacks on the styling of
+   the menu.
+
+   - Solution: Use JavaScript to re-position the menu html code into
+   the header when the website is viewed on larger screens and then moved back
+   to its orginal place for mobile screens.
+*/
+
+/*Plan:
+ - If statement checking to see if the browser width is than 960px
+ - If the condition is true we want to get the navigation menu ul
+ - Append the navigation to the header*/
+
+
+
+
+
+
+
+
+if($(window).width() >= 960){
+  $navMenu.appendTo('.nav .wrapper');
+}
+
+$(window).resize(function(){
+  if($(this).width() >= 960){
+    $navMenu.appendTo('.nav .wrapper');
+  }
+  else {
+    navMenu.insertBefore('.header__masthead');
+  }
 
 });
 
-if($(window).resize().width() >= 960){
-  $('.nav__list').appendTo('.nav .wrapper');
-}
-/*else if($(window).width() < 960 && $('.nav__list').parent('.wrapper')) {
-  $('.nav__list').prepend('.header');
+
+/*
+- Check whether the browser window has been resized less than 960px at any point after
+the browser window width has been 960px and over.
+ - Check the browser width less than 960
+ - Check if the navMenu parent is header*/
+/*if ($(window).resize().width < 960){ //&& $('.wrapper').has(navMenu)){
+  //navMenu.insertBefore('.header');
+  console.log('Hi guys');
 }*/
+
+
+/*
+ - If the condition is true then we want to move the navigation menu ul back
+ to its orginal place.
+
+ */
