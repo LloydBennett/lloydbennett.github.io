b5a711a4ece8f43f10577cd754380b8bafb30bad
LloydBennett <lj_bennett@hotmail.co.uk>
2019-06-26 23:41:56 +0100

    Created loading timer calculator method

--- js/loader.js
+++ js/loader.js
@@ -4,12 +4,15 @@ class Loader {
     this.loader = document.querySelector('[data-loader]');
     this.loaderBgStrips = document.querySelectorAll('.loader__bg .background-strip');
     this.heroBgStrips = document.querySelectorAll('[data-hero-strip]');
-    this.loaderTimer = document.querySelector('[data-loader-timer]');
+    this.loaderTimerContent = document.querySelectorAll('.loader__percentage');
     this.heroTitles = document.querySelectorAll('[data-hero-titles]');
     this.heroCopy = document.querySelector('[data-hero-copy]');
     this.heroBtn = document.querySelector('[data-hero-cta]');
     this.heroBtnLine = document.querySelector('[data-hero-cta] .button__line');
     this.metaInfo = document.querySelector('[data-meta-info]');
+    this.loaderPercentage = document.querySelector('[data-loader-percentage]');
+    this.finishedCalculating = false;
+
     this.init();
   }
 
@@ -18,19 +21,10 @@ class Loader {
   }
 
   animateLoader() {
-    var offsetDelay = "-=0.15";
-    console.log(this.heroBtnLine);
-    // pull in the percentage timer up
-    // animate the timer to show how much has been loaded
-    // hide the loader
-    this.animateNodeList(this.loaderBgStrips, { height: 0 }, offsetDelay);
-    this.animateNodeList(this.heroBgStrips, { height: "100%" }, offsetDelay);
-    this.loaderTl.to(this.loader, 0.3, { opacity: 0, visibility: "hidden" });
-    this.animateNodeList(this.heroTitles, { y: 0 }, "-=0.22");
-    this.loaderTl.fromTo(this.heroCopy, 0.3, { opacity: 0, y: 50 }, { opacity: 1, y: 0 });
-    this.loaderTl.fromTo(this.heroBtn, 0.3, { opacity: 0 }, { opacity: 1 }, "last-elements");
-    this.loaderTl.fromTo(this.heroBtnLine, 0.05, { width: 0 }, { width: "100%" }, offsetDelay);
-    this.loaderTl.fromTo(this.metaInfo, 0.6, { opacity: 0}, {opacity: 1}, "last-elements");
+    this.animateNodeList(this.loaderTimerContent, { y: 0 }, "-=0.3");
+
+    //call this function at the end of this animation
+    this.calculateLoadTime(0);
   }
 
   animateNodeList(nodelist, props, offset) {
@@ -43,51 +37,37 @@ class Loader {
       }
     });
   }
+
+  calculateLoadTime(al) {
+    this.loaderPercentage.innerHTML = al;
+    al++;
+
+    let timer = setTimeout(() => {
+      this.calculateLoadTime(al);
+    }, 40);
+
+    if(al == 100) {
+      clearInterval(timer);
+      this.loaderPercentage.innerHTML = 100;
+      this.revealContent();
+    }
+  }
+
+  revealContent() {
+    let offsetDelay = "-=0.15";
+
+    this.animateNodeList(this.loaderTimerContent, { y: "100%" }, "-=0.3");
+    this.animateNodeList(this.loaderBgStrips, { height: 0 }, offsetDelay);
+    this.animateNodeList(this.heroBgStrips, { height: "100%" }, offsetDelay);
+    this.loaderTl.to(this.loader, 0.3, { opacity: 0, visibility: "hidden" });
+    this.animateNodeList(this.heroTitles, { y: 0 }, "-=0.22");
+    this.loaderTl.fromTo(this.heroCopy, 0.3, { opacity: 0, y: 50 }, { opacity: 1, y: 0 });
+    this.loaderTl.fromTo(this.heroBtn, 0.3, { opacity: 0 }, { opacity: 1 }, "last-elements");
+    this.loaderTl.fromTo(this.heroBtnLine, 0.05, { width: 0 }, { width: "100%" }, offsetDelay);
+    this.loaderTl.fromTo(this.metaInfo, 0.6, { opacity: 0}, {opacity: 1}, "last-elements");
+  }
 }
 
 window.addEventListener('load', (event) => {
   new Loader();
 });
-
-  // // percentage loader reaches 100%;
-  // //   // background strip slides up
-  // //   // loader disappears
-  // var loaderPercentage = document.querySelector('[data-percentage-value]');
-  // var percentageValue = loaderPercentage.dataset.percentageValue;
-  // var backgroundStrips = document.querySelectorAll('[data-hero-strip]');
-  //
-  // function loaderTimer() {
-  //   setInterval(function() {
-  //     if(percentageValue === 100) {
-  //       clearInterval();
-  //       animateLoader();
-  //       return;
-  //     } else {
-  //       percentageValue++;
-  //       loaderPercentage.innerHTML = percentageValue;
-  //     }
-  //   }, 10);
-  // }
-  //
-  // function animateLoader() {
-  //   var loader = document.querySelector('.loader');
-  //
-  //   loader.addEventListener("transitionend", function() {
-  //     backgroundStrips.forEach(function(elem){
-  //       console.log(elem);
-  //       //elem.classList.add('bg-grow');
-  //     });
-  //   });
-  //
-  //   loader.classList.add('loader--hide');
-  // }
-  //
-  // function loader () {
-  //   loaderPercentage.addEventListener("animationend", function() {
-  //     setTimeout(loaderTimer, 800);
-  //   });
-  // }
-  //
-  // window.addEventListener('load', function(){
-  //   loader();
-  // });
