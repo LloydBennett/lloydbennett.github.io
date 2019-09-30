537a76ee42a2b42fd87a3f89b66645fed46a60ab
LloydBennett <lj_bennett@hotmail.co.uk>
2019-09-30 17:17:34 +0100

    Removed loader component
    Not needed

--- js/loader.js
+++ js/loader.js
@@ -1,16 +1,13 @@
 class Loader {
   constructor(){
     this.loaderTl = new TimelineLite();
-    this.loader = document.querySelector('[data-loader]');
-    this.loaderBgStrips = document.querySelectorAll('.loader__bg .background-strip');
     this.heroBgStrips = document.querySelectorAll('[data-hero-strip]');
-    this.loaderTimerContent = document.querySelectorAll('.loader__percentage');
     this.heroTitles = document.querySelectorAll('[data-hero-titles]');
     this.heroCopy = document.querySelector('[data-hero-copy]');
     this.heroBtn = document.querySelector('[data-hero-cta]');
     this.heroBtnLine = document.querySelector('[data-hero-cta] .button__line');
     this.metaInfo = document.querySelector('[data-meta-info]');
-    this.loaderPercentage = document.querySelector('[data-loader-percentage]');
+    this.footer = document.query('[data-footer]');
     this.finishedCalculating = false;
 
     this.init();
@@ -18,13 +15,6 @@ class Loader {
 
   init() {
     this.animateLoader();
-  }
-
-  animateLoader() {
-    this.animateNodeList(this.loaderTimerContent, { y: 0 }, "-=0.3");
-
-    //call this function at the end of this animation
-    this.calculateLoadTime(0);
   }
 
   animateNodeList(nodelist,props, offset, duration = 0.3) {
@@ -38,28 +28,10 @@ class Loader {
     });
   }
 
-  calculateLoadTime(al) {
-    this.loaderPercentage.innerHTML = al;
-    al++;
-
-    let timer = setTimeout(() => {
-      this.calculateLoadTime(al);
-    }, 40);
-
-    if(al == 100) {
-      clearInterval(timer);
-      this.loaderPercentage.innerHTML = 100;
-      this.revealContent(); //set a delay to function
-    }
-  }
-
-  revealContent() {
+  animateLoader() {
     let offsetDelay = "-=0.15";
 
-    this.animateNodeList(this.loaderTimerContent, { y: "100%" },"-=0.6", 0.6);
-    this.animateNodeList(this.loaderBgStrips, { height: 0 }, offsetDelay);
     this.animateNodeList(this.heroBgStrips, { height: "100%" }, offsetDelay);
-    this.loaderTl.to(this.loader, 0.3, { opacity: 0, visibility: "hidden" });
     this.animateNodeList(this.heroTitles, { y: 0 }, "-=0.22");
     this.loaderTl.fromTo(this.heroCopy, 0.3, { opacity: 0, y: 50 }, { opacity: 1, y: 0 });
     this.loaderTl.fromTo(this.heroBtn, 0.3, { opacity: 0 }, { opacity: 1 }, "last-elements");
