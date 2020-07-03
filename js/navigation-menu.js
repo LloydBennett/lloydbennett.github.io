7a06b4a27415de0cb987344a6d83aec763821ee4
LloydBennett <lj_bennett@hotmail.co.uk>
2020-07-03 01:18:36 +0100

    First pass functionality of submenu

--- js/navigation-menu.js
+++ js/navigation-menu.js
@@ -4,9 +4,15 @@ function NavigationMenu(parent, parentLinks, menuTrigger, navBar) {
   this.navBar = navBar;
   this.menuTrigger = menuTrigger;
   this.menuIsOpen = false;
+  this.subMenuOpen = false;
   this.isAnimating = false;
   this.body = $('[data-body]');
-
+  this.subMenuTrigger = document.querySelector('[data-toggle-sub-menu]');
+  this.closeSubMenuTrigger = document.querySelector('[data-close-submenu]');
+  this.metaFrame = document.querySelector('[data-meta-frame]');
+  this.subMenu = document.querySelector('[data-sub-menu]');
+  this.mainMenu = document.querySelector('[data-main-menu]');
+  this.logo = document.querySelector('[data-logo]');
   this.init();
 }
 
@@ -55,15 +61,29 @@ NavigationMenu.prototype = {
 
   },
   addEvents: function() {
-    var _this = this;
+    let _this = this;
     this.menuTrigger.forEach((el) => {
       el.addEventListener('click', () => {
         _this.checkNavigationMenuState();
       });
     });
 
+    this.subMenuTrigger.addEventListener('click', (e) => {
+      e.preventDefault();
+      this.toggleSubMenu(this);
+    });
 
-
+    this.closeSubMenuTrigger.addEventListener('click', (e) => {
+      e.preventDefault();
+      this.toggleSubMenu(this);
+    });
+  },
+  toggleSubMenu:(_this) => {
+    _this.metaFrame.classList.toggle('flip-frame');
+    _this.subMenu.classList.toggle('appear');
+    _this.mainMenu.classList.toggle('hide');
+    _this.closeSubMenuTrigger.classList.toggle('appear');
+    _this.logo.classList.toggle('hide');
   },
   smoothScrolling: scrollToSection,
   init: function() {
