b3d8518a2137f4a9b8e3c0ac1aacd7f0fa4c1a73
LloydBennett <lj_bennett@hotmail.co.uk>
2020-07-10 13:34:10 +0100

    WIP title animation

--- js/loader.js
+++ js/loader.js
@@ -1,19 +1,22 @@
 class Loader {
   constructor(){
     this.loaderTl = new TimelineLite();
-    this.heroBgStrips = document.querySelectorAll('[data-hero-strip]');
-    this.heroTitles = document.querySelectorAll('[data-hero-titles]');
-    this.heroCopy = document.querySelector('[data-hero-copy]');
-    this.heroBtn = document.querySelector('[data-hero-cta]');
-    this.heroBtnLine = document.querySelector('[data-hero-cta] .button__line');
-    this.metaInfo = document.querySelector('[data-meta-info]');
-    this.footer = document.querySelector('[data-footer]');
-    this.heroImage = document.querySelector('[data-hero-image]');
-    this.heroHorizLine = document.querySelector('[data-hero-hr]');
-    this.heroDetailsListItems = document.querySelectorAll('[data-hero-details-item]');
-    this.heroSocials = document.querySelector('[data-hero-socials]');
-    this.navBar = document.querySelector('[data-nav-bar]');
-    this.finishedCalculating = false;
+    this.heroTitle = document.querySelector('[data-hero-title]');
+    this.heroTitleLetters = this.heroTitle.querySelectorAll('.word span');
+
+    // this.heroBgStrips = document.querySelectorAll('[data-hero-strip]');
+    // this.heroTitles = document.querySelectorAll('[data-hero-titles]');
+    // this.heroCopy = document.querySelector('[data-hero-copy]');
+    // this.heroBtn = document.querySelector('[data-hero-cta]');
+    // this.heroBtnLine = document.querySelector('[data-hero-cta] .button__line');
+    // this.metaInfo = document.querySelector('[data-meta-info]');
+    // this.footer = document.querySelector('[data-footer]');
+    // this.heroImage = document.querySelector('[data-hero-image]');
+    // this.heroHorizLine = document.querySelector('[data-hero-hr]');
+    // this.heroDetailsListItems = document.querySelectorAll('[data-hero-details-item]');
+    // this.heroSocials = document.querySelector('[data-hero-socials]');
+    // this.navBar = document.querySelector('[data-nav-bar]');
+    // this.finishedCalculating = false;
 
     this.init();
   }
@@ -37,25 +40,27 @@ class Loader {
     let offsetDelay = "-=0.15";
     let homePage = document.getElementById('home');
     let caseStudy = document.querySelector('.casestudy');
+    console.log(this.heroTitleLetters);
+    this.animateNodeList(this.heroTitleLetters, { y: 0 }, "-=0.39", 0.4);
 
-    this.animateNodeList(this.heroBgStrips, { height: "100%" }, offsetDelay);
-    this.animateNodeList(this.heroTitles, { y: 0 }, "-=0.22");
-
-    if(homePage) {
-      this.loaderTl.fromTo(this.heroCopy, 0.3, { opacity: 0, y: 50 }, { opacity: 1, y: 0 });
-      this.loaderTl.fromTo(this.heroBtn, 0.3, { opacity: 0 }, { opacity: 1 }, "last-elements");
-      this.loaderTl.fromTo(this.heroBtnLine, 0.3, { width: 0 }, { width: "100%" }, offsetDelay);
-    }
-    else if(caseStudy) {
-      this.loaderTl.fromTo(this.heroImage, 0.3, { opacity: 0, x: 50 }, { opacity: 1, x: 0 });
-      this.loaderTl.fromTo(this.heroHorizLine, 0.3, { width: 0 }, { width: "100%" }, "last-elements");
-      this.animateNodeList(this.heroDetailsListItems, { y: 0 }, "-=0.05", 0.2);
-    }
-
-    this.loaderTl.fromTo(this.metaInfo, 0.6, { opacity: 0}, { opacity: 1 }, "last-elements");
-    this.loaderTl.fromTo(this.heroSocials, 0.6, { opacity: 0}, { opacity: 1 }, "last-elements");
-    this.loaderTl.fromTo(this.navBar, 0.6, { opacity: 0}, { opacity: 1 }, "last-elements");
-    this.loaderTl.fromTo(this.footer, 0.3, { opacity: 0 }, { opacity: 1 });
+    // this.animateNodeList(this.heroBgStrips, { height: "100%" }, offsetDelay);
+    // this.animateNodeList(this.heroTitles, { y: 0 }, "-=0.22");
+    //
+    // if(homePage) {
+    //   this.loaderTl.fromTo(this.heroCopy, 0.3, { opacity: 0, y: 50 }, { opacity: 1, y: 0 });
+    //   this.loaderTl.fromTo(this.heroBtn, 0.3, { opacity: 0 }, { opacity: 1 }, "last-elements");
+    //   this.loaderTl.fromTo(this.heroBtnLine, 0.3, { width: 0 }, { width: "100%" }, offsetDelay);
+    // }
+    // else if(caseStudy) {
+    //   this.loaderTl.fromTo(this.heroImage, 0.3, { opacity: 0, x: 50 }, { opacity: 1, x: 0 });
+    //   this.loaderTl.fromTo(this.heroHorizLine, 0.3, { width: 0 }, { width: "100%" }, "last-elements");
+    //   this.animateNodeList(this.heroDetailsListItems, { y: 0 }, "-=0.05", 0.2);
+    // }
+    //
+    // this.loaderTl.fromTo(this.metaInfo, 0.6, { opacity: 0}, { opacity: 1 }, "last-elements");
+    // this.loaderTl.fromTo(this.heroSocials, 0.6, { opacity: 0}, { opacity: 1 }, "last-elements");
+    // this.loaderTl.fromTo(this.navBar, 0.6, { opacity: 0}, { opacity: 1 }, "last-elements");
+    // this.loaderTl.fromTo(this.footer, 0.3, { opacity: 0 }, { opacity: 1 });
   }
 }
 
