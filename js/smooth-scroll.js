4f8788ad99ef4ac311f8caade0cb732c5e8b67d4
LloydBennett <lj_bennett@hotmail.co.uk>
2020-11-27 20:58:33 +0000

    Added animated scroll affect

--- js/smooth-scroll.js
+++ js/smooth-scroll.js
@@ -1,111 +1,116 @@
-const MathUtils = {
-    // map number x from range [a, b] to [c, d]
-    map: (x, a, b, c, d) => (x - a) * (d - c) / (b - a) + c,
-    // linear interpolation
-    lerp: (a, b, n) => (1 - n) * a + n * b
-};
-
-const body = document.body;
-
-let winsize;
-let docScroll;
-
-const calcWinsize = function(){
-  winsize = {
-    width: window.innerWidth,
-    height: window.innerHeight
-  };
-  return winsize;
+const config = {
+  height: window.innerHeight,
+  width: window.innerWidth
 }
-
-const getPageYScroll = function() {
-  return docScroll = window.pageYOffset || document.documentElement.scrollTop;
-}
-
-
-calcWinsize();
-
-window.addEventListener('resize', calcWinsize);
-window.addEventListener('scroll', getPageYScroll);
-
 
 class SmoothScroll {
   constructor() {
-    console.log('heyy smooth!!');
+    this.bindMethods();
 
-    this.domElems = {
-      main: document.querySelector('[ data-main ]')
-    };
-
-    this.domElems.scrollable = this.domElems.main.querySelector('[data-scroll]');
-    this.domElems.content = this.domElems.main.querySelectorAll('[data-item]');
-    console.log(this.domElems.scrollable);
-    this.items = [];
-    this.domElems.content.forEach((item) => {
-      this.items.push(item);
-      //this.items.push(new Item(item));
-    });
-
-    this.renderedStyles = {
-      translationY: {
-          previous: 0,
-          current: 0,
-          ease: 0.1,
-          setValue: () => docScroll
-      }
-    };
-
-    this.update();
-    // previous = MathUtils.lerp(previous, current, ease)
-    this.setSize();
-    this.style();
-    this.initEvents();
-    requestAnimationFrame(() => this.render());
-  }
-
-  update() {
-    for (const key in this.renderedStyles ) {
-      this.renderedStyles[key].current = this.renderedStyles[key].previous = this.renderedStyles[key].setValue();
-    }
-    this.layout();
-  }
-
-  layout() {
-    this.domElems.scrollable.style.transform = `translate3d(0,${-1*this.renderedStyles.translationY.previous}px,0)`;
-  }
-
-  setSize() {
-    body.style.height = this.domElems.scrollable.scrollHeight + 'px';
-  }
-
-  style() {
-    this.domElems.main.style.position = 'fixed';
-    this.domElems.main.style.width = this.domElems.main.style.height = '100%';
-    this.domElems.main.style.top = this.domElems.main.style.left = 0;
-    this.domElems.main.style.overflow = 'hidden';
-  }
-
-  initEvents() {
-    window.addEventListener('resize', () => this.setSize());
-  }
-
-  render() {
-    for (const key in this.renderedStyles ) {
-        this.renderedStyles[key].current = this.renderedStyles[key].setValue();
-        this.renderedStyles[key].previous = MathUtils.lerp(this.renderedStyles[key].previous, this.renderedStyles[key].current, this.renderedStyles[key].ease);
-    }
-    this.layout();
-
-    // for every item
-    for (const item of this.items) {
-      // if the item is inside the viewport call it's render function
-      // this will update the item's inner image translation, based on the document scroll value and the item's position on the viewport
-      if ( item.isVisible ) {
-        item.render();
-      }
+    this.data = {
+      ease: 0.1,
+      current: 0,
+      last: 0,
+      rounded: 0
     }
 
-    // loop..
-    requestAnimationFrame(() => this.render());
+    this.dom = {
+      el: document.querySelector('[data-main]'),
+      content: document.querySelector('[data-scroll]')
+    }
+
+    this.rAF = null
+
+    this.init()
+  }
+
+  bindMethods() {
+    ['scroll', 'run', 'resize']
+    .forEach((fn) => this[fn] = this[fn].bind(this))
+  }
+
+  setStyles() {
+    Object.assign(this.dom.el.style, {
+      position: 'fixed',
+      top: 0,
+      left: 0,
+      height: '100%',
+      width: '100%',
+      overflow: 'hidden'
+    })
+  }
+
+  setHeight() {
+    document.body.style.height = `${this.dom.content.getBoundingClientRect().height}px`
+  }
+
+  resize() {
+    this.setHeight()
+    this.scroll()
+  }
+
+  scroll() {
+    this.data.current = window.scrollY
+  }
+
+  run() {
+    this.data.last += (this.data.current - this.data.last) * this.data.ease
+    this.data.rounded = Math.round(this.data.last * 100) / 100
+
+    const diff = this.data.current - this.data.rounded
+    const acc = diff / config.width
+
+    this.dom.content.style.transform = `translate3d(0, -${this.data.rounded}px, 0)`;
+    this.requestAnimationFrame();
+  }
+
+  on() {
+    this.setStyles()
+    this.setHeight()
+    this.addEvents()
+
+    this.requestAnimationFrame()
+  }
+
+  off() {
+    this.cancelAnimationFrame()
+
+    this.removeEvents()
+  }
+
+  requestAnimationFrame() {
+    this.rAF = requestAnimationFrame(this.run)
+  }
+
+  cancelAnimationFrame() {
+    cancelAnimationFrame(this.rAF)
+  }
+
+  destroy() {
+    document.body.style.height = '';
+
+    this.data = null;
+
+    this.removeEvents()
+    this.cancelAnimationFrame()
+  }
+
+  resize() {
+    this.setHeight()
+    this.data.rounded = this.data.last = this.data.current;
+  }
+
+  addEvents() {
+    window.addEventListener('resize', this.resize, { passive: true })
+    window.addEventListener('scroll', this.scroll, { passive: true })
+  }
+
+  removeEvents() {
+    window.removeEventListener('resize', this.resize, { passive: true })
+    window.removeEventListener('scroll', this.scroll, { passive: true })
+  }
+
+  init() {
+    this.on()
   }
 }
