85686424e0e985ba9905599557fa12cc6411eac9
LloydBennett <lj_bennett@hotmail.co.uk>
2021-01-09 03:30:48 +0000

    Added scroll functionality when you click on cta

--- js/main.js
+++ js/main.js
@@ -5,6 +5,28 @@ function initializeFunctionality() {
   new Form();
 }
 
-window.addEventListener('load', function(){
+window.addEventListener('load', function() {
+  let scrollToSectionTrig = document.querySelectorAll('[data-scroll-to-section]');
+
+  function scrollToSection(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    let href = this.getAttribute("href");
+    let hrefElement = document.querySelector(href);
+
+    let destination = hrefElement.getBoundingClientRect().top;
+
+    window.scrollTo({
+      top: destination,
+      left: 0,
+      behaviour: 'smooth'
+    });
+  }
+
+  scrollToSectionTrig.forEach((item) => {
+    item.addEventListener('click', scrollToSection);
+  });
+
   initializeFunctionality();
 });
